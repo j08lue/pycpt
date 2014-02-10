ba385c1864bf40e4d45cf057ab16309f0dfa7339
j08lue <j08lue@gmail.com>
2014-02-10 10:18:54 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name = 'pycpt',
+      version = '0.1.0',
+      author = 'Jonas Bluethgen',
+      author_email = 'bluthgen@nbi.ku.dk',
+      packages = ['pycpt'],
+      url = 'http://www.gfy.ku.dk/~bluthgen',
+      license = 'LICENSE.txt',
+      description = 'Read, modify, and display color maps from .cpt files',
+      long_description = open('README.md').read(),
+      )
