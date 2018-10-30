e0daf808fcfebfae3ca830472dbe7818414e1319
Jonas Sølvsteen <j08lue@gmail.com>
2018-10-30 10:40:07 +0100

    setup and travis

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
+import os
 
-setup(name = 'pycpt',
-      version = '0.2.0',
-      author = 'Jonas Bluethgen',
-      author_email = 'bluthgen@nbi.ku.dk',
-      packages = ['pycpt'],
-      url = 'http://www.gfy.ku.dk/~bluthgen',
-      license = 'LICENSE.txt',
-      description = 'Read, modify, and display color maps from .cpt files',
-      long_description = open('README.md').read(),
-      )
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as fp:
+    install_requires = fp.read()
+
+setup(
+    name='pycpt',
+    version='0.2.0',
+    author='Jonas Sølvsteen',
+    author_email='j08lue@gmail.com',
+    packages=find_packages(),
+    description='Read, modify, and display color maps from .cpt files',
+    install_requires=install_requires,
+    extras_require={
+        'test': [
+            'pytest>=3.5',
+            'pytest-cov',
+            'codecov'
+        ]
+    }
+)
